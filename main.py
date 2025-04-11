4b09feef6d463c15753fcc74fba9c4f43da87e16
Justasenbtw <lisunkov.stas@bk.ru>
2025-04-11 22:33:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def main():
     name = "Lisunkov Stas"
-    current_date = datetime.now().strftime("%d.%m.%Y")
+    current_date = datetime.now().strftime("%d.%m.%Y %H:%M")
 
     print(f"Name: {name}")
     print(f"Date: {current_date}")
