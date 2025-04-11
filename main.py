f08ac6ebed94133029f8c4ac8ba0ca20b8591bb5
Justasenbtw <lisunkov.stas@bk.ru>
2025-04-11 22:07:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
 def main():
-    name = "Лисунков Стас"
+    name = "Lisunkov Stas"
     current_date = datetime.now().strftime("%d.%m.%Y")
 
-    print(f"Имя: {name}")
-    print(f"Дата: {current_date}")
+    print(f"Name: {name}")
+    print(f"Date: {current_date}")
 
 if __name__ == "__main__":
     main()
